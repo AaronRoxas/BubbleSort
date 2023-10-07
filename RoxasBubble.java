0293984b5187a394eb345fb0445c185714e6d53c
AaronRoxas <93905441+AaronRoxas@users.noreply.github.com>
2023-10-07 19:43:45 +0800

    added binary searching

--- RoxasBubble.java
+++ RoxasBubble.java
@@ -3,7 +3,7 @@ public class bubbleSort
 {
     public static void main(String[] args) 
     {
-      Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.print("Enter array size: ");
         int size = s.nextInt();
         int array[] = new int [size];
@@ -14,6 +14,38 @@ public class bubbleSort
             array[i] = s.nextInt();
         }
         sort(array);
+        System.out.print("Enter value to find: ");
+        int val = s.nextInt();
+        search(array,val);
+
+    }
+    //Method to do Binary Search
+    static void search(int array[], int target) 
+    {
+        int size = array.length;
+        int lowBound = 0;  
+        int highBound = size - 1; 
+        String display ="Value not found";
+
+        while (lowBound <= highBound) 
+        {
+            int mid = lowBound + (highBound - lowBound) / 2;
+
+            if (array[mid] < target)
+            {
+                lowBound = mid + 1;
+            } 
+            else if (array[mid] > target)
+            {
+                highBound = mid - 1;
+            } 
+            else
+            {
+                display = target + " found at index " + mid;
+                break;
+            }
+        }
+        System.out.println(display);
     }
     
     static void sort(int array[])
@@ -27,7 +59,6 @@ public class bubbleSort
         {
             unsorted += array[i]+" ";
         }
-        
         //Loop to sort array
         for(int i = 0; i <size-1;i++)
         {
